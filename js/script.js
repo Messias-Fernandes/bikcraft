c0a06991743e7e89eb08220ba408c98c0e8de890
Messias-Fernandes <fr.messias27@gmail.com>
2024-04-01 17:47:35 -0300

    Ativar parâmetros de URL do orçamento

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+// Ativar links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -8,4 +10,20 @@ function ativarLink(link) {
     }
 }
 
-links.forEach(ativarLink)
+links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro) ;
+
+     if (elemento) {
+        elemento.checked = true
+     }
+}
+
+parametros.forEach(ativarProduto);
+
+console.log(parametros);
